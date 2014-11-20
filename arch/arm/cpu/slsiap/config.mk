041a917b03c92b69d1af8f81af9c82044879d62e
jhkim <jhkim@nexell.co.kr>
2014-11-20 10:17:08 +0900

    MOD: build error when export CROSS_COMPILE

--- arch/arm/cpu/slsiap/config.mk
+++ arch/arm/cpu/slsiap/config.mk
@@ -4,3 +4,9 @@
 # =========================================================================
 SOCDIR=arch/$(ARCH)/cpu$(if $(CPU),/$(CPU),)$(if $(SOC),/$(SOC),)
 LDPPFLAGS += -DSOCDIR=$(SOCDIR)
+
+#
+# avoid build error complict with multiple defined rasie,,,, in eabi_compat.c
+# when export CROSS_COMPILE	(arch/arm/config.mk)
+#
+PLATFORM_LIBS :=

eac4ed45f2b6e707eec80b2ea838f37d901f17e9
Youngbok Park <ybpark@nexell.co.kr>
2014-12-04 10:23:11 +0900

    mod 	mmc clock dividor calculate function.

--- drivers/mmc/dw_mmc.c
+++ drivers/mmc/dw_mmc.c
@@ -240,7 +240,8 @@ static int dwmci_setup_bus(struct dwmci_host *host, u32 freq)
 		return -EINVAL;
 	}
 
-	div = DIV_ROUND_UP(sclk, 2 * freq);
+	//div = DIV_ROUND_UP(sclk, 2 * freq);
+	div = (sclk / (2*freq));
 
 	dwmci_writel(host, DWMCI_CLKENA, 0);
 	dwmci_writel(host, DWMCI_CLKSRC, 0);

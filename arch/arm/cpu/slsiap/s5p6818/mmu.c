902f97f1ea75adaac62f8de952bb57451031c69b
Sungwoo Park <swpark@nexell.co.kr>
2015-02-13 18:53:42 +0900

    [FIX] bootdelay zero console death problem when CONFIG_SMP on

--- arch/arm/cpu/slsiap/s5p6818/mmu.c
+++ arch/arm/cpu/slsiap/s5p6818/mmu.c
@@ -134,7 +134,7 @@ static void make_page_table(u32 *ptable)
 	index = 0;
 	mode = (0<<SECTION_CACHEABLE) | (0<<SECTION_BUFFERABLE) | (FLD_SECTION<<0);	// No Cachable & No Bufferable
 #ifdef SMP_CACHE_COHERENCY
-	mode = 0xC16;
+	mode = 0xC06;		// 0xC16 -> SBZ
 #else
 	mode = mode | AP_CLIENT<<SECTION_AP;					// set kernel R/W permission
 #endif

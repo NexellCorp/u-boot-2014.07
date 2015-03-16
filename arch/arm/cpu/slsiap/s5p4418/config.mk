70d7021beb55c091b171aa24cf9c239f849c39dd
KOO Bon-Gyu <freestyle@nexell.co.kr>
2015-03-16 11:00:22 +0900

    [FTL] modify build config for license test
    Signed-off-by: KOO Bon-Gyu <freestyle@nexell.co.kr>

--- arch/arm/cpu/slsiap/s5p4418/config.mk
+++ arch/arm/cpu/slsiap/s5p4418/config.mk
@@ -67,7 +67,17 @@ endif
 #   EWS FTL Build Option
 # =========================================================================
 ARCH_CFLAGS += -D__SUPPORT_MIO_UBOOT__
-ARCH_CFLAGS += -D__SUPPORT_MIO_UBOOT_CHIP_S5P4418__
+ifeq ($(CONFIG_MACH_S5P4418),y)
+    ARCH_CFLAGS += -D__SUPPORT_MIO_UBOOT_CHIP_S5P4418__
+else
+    ifeq ($(CONFIG_MACH_S5P6818),y)
+        ARCH_CFLAGS += -D__SUPPORT_MIO_UBOOT_CHIP_S5P6818__
+    else
+        ARCH_CFLAGS += -D__SUPPORT_MIO_UBOOT_CHIP_NXP4330__
+    endif
+endif
+
+
 
 # =========================================================================
 #	Build options for HOSTCC

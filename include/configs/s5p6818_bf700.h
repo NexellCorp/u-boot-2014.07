0804eb762c357d78c150b042e108030b22ce72fe
jhkim <jhkim@nexell.co.kr>
2015-03-24 15:42:13 +0900

    MOD: default bootcommand

--- include/configs/s5p6818_bf700.h
+++ include/configs/s5p6818_bf700.h
@@ -132,8 +132,7 @@
 #define CONFIG_GATEWAYIP				192.168.1.254
 #define CONFIG_BOOTFILE					"uImage"  		/* File to load	*/
 
-//#define CONFIG_BOOTCOMMAND "ext4load mmc 0:1 0x48000000 uImage;ext4load mmc 0:1 0x49000000 root.img.gz;bootm 0x48000000"
-#define CONFIG_BOOTCOMMAND "mmc dev 0;mmcinfo;fatload mmc 0:1 0x48000000 uImage;bootm 0x48000000"
+#define CONFIG_BOOTCOMMAND "ext4load mmc 2:1 0x48000000 uImage;ext4load mmc 2:1 0x49000000 root.img.gz;bootm 0x48000000"
 
 /*-----------------------------------------------------------------------
  * Miscellaneous configurable options

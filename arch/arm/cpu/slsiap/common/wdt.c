26d6dbf75c0a70f2719ec049be1eb53ef3dbb062
Hyunseok Jung <hsjung@nexell.co.kr>
2015-07-27 22:10:48 +0900

    [EDIT:WDT] 1. add default timeout if config not defined. 2. edit timeout value calcuration.

--- arch/arm/cpu/slsiap/common/wdt.c
+++ arch/arm/cpu/slsiap/common/wdt.c
@@ -6,6 +6,10 @@
 #include <asm/errno.h>
 #include <platform.h>
 
+#ifndef CONFIG_WATCHDOG_TIMEOUT
+#define CONFIG_WATCHDOG_TIMEOUT	10
+#endif
+
 void hw_watchdog_reset(void)
 {
 	// reload count value
@@ -19,7 +23,7 @@ void hw_watchdog_restart(void)
 {
 	// enable hw watchdog
 	if (!NX_WDT_GetEnable(0)) {
-		NX_WDT_SetCurrentCount(0, CONFIG_WATCHDOG_TIMEOUT * 25/* about 1 sec */);
+		NX_WDT_SetCurrentCount(0, CONFIG_WATCHDOG_TIMEOUT * 6103/* about 1 sec */);
 		NX_WDT_SetResetEnable(0, CTRUE);
 		NX_WDT_SetEnable(0, CTRUE);
 	} 

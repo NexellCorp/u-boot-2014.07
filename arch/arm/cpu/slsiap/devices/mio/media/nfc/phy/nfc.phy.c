3b3f76472463ffc2c4d2dec7073ca97a8bff8b96
KOO Bon-Gyu <freestyle@nexell.co.kr>
2015-07-29 19:24:40 +0900

    [FTL] aarch64 io burst x4
    Signed-off-by: KOO Bon-Gyu <freestyle@nexell.co.kr>

--- arch/arm/cpu/slsiap/devices/mio/media/nfc/phy/nfc.phy.c
+++ arch/arm/cpu/slsiap/devices/mio/media/nfc/phy/nfc.phy.c
@@ -83,7 +83,7 @@
 #define IO_BURST_X0		(0x1)
 
 #if defined(CONFIG_ARM64)			/* FIXME: u-boot cannot recognize__COMPILE_MODE_X64__ */
-#define IOR_WIDTH		IO_BURST_X0
+#define IOR_WIDTH		IO_BURST_X4
 #define IOW_WIDTH		IO_BURST_X0
 #else
 #define IOR_WIDTH		IO_BURST_X4
@@ -95,15 +95,25 @@
 /******************************************************************************
  * IO Read/Write Burst
  ******************************************************************************/
+#ifdef CONFIG_ARM64
+void __nfc_read_burst(const void __iomem *addr, void *data, int len);
+void __nfc_write_burst(const void __iomem *addr, void *data, int len);
+
+static void ior_burst(char *data, void __iomem *nfdata, int len)
+{
+	__nfc_read_burst(nfdata, data, len);
+}
+
+static void iow_burst(char *data, void __iomem *nfdata, int len)
+{
+	__nfc_write_burst(nfdata, data, len);
+}
+#else
 void __nfc_readsb(const void __iomem *addr, void *data, int bytelen);
 void __nfc_readsl(const void __iomem *addr, void *data, int longlen);
 void __nfc_writesb(void __iomem *addr, const void *data, int bytelen);
 void __nfc_writesl(void __iomem *addr, const void *data, int longlen);
 
-#ifdef CONFIG_ARM64
-#define ior_burst(...)		do {} while (0)
-#define iow_burst(...)		do {} while (0)
-#else
 static void ior_burst(char *data, void __iomem *nfdata, int len)
 {
 	int burst_len = len & ~(4-1);

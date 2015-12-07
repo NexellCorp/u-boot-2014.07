c605d4aec21fbfae662e49b266df42fae19fe0d3
Bongkwan Kook <kook@nexell.co.kr>
2015-12-07 14:31:15 +0900

    [Add] - for Smartbox. (micrel PHY)

--- arch/arm/cpu/slsiap/devices/gpio_nxp.c
+++ arch/arm/cpu/slsiap/devices/gpio_nxp.c
@@ -23,7 +23,238 @@
 #include <asm/io.h>
 #include <asm/gpio.h>
 
+#if (0)
+#define DBGOUT(msg...)		{ printf("BD: " msg); }
+#else
+#define DBGOUT(msg...)		do {} while (0)
+#endif
+
+void nxp_gpio_set_alt(int gpio, int mode)
+{
+	int grp, bit;
+
+	if (gpio > (PAD_GPIO_ALV - 1) )
+		return;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	NX_GPIO_SetPadFunction(grp, bit, (NX_GPIO_PADFUNC)mode);
+}
+
+int nxp_gpio_get_alt(int gpio)
+{
+	int grp, bit;
+
+	if (gpio > (PAD_GPIO_ALV - 1) )
+		return -1;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	return (int) NX_GPIO_GetPadFunction(grp, bit);
+}
+
+void nxp_gpio_direction_output(int gpio, int val)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	if (gpio < PAD_GPIO_ALV)
+	{
+		NX_GPIO_SetOutputValue(grp, bit, (val ? CTRUE : CFALSE));
+		NX_GPIO_SetOutputEnable(grp, bit, CTRUE);
+	}
+	else
+	{
+		NX_ALIVE_SetOutputValue(bit, (val ? CTRUE : CFALSE));
+		NX_ALIVE_SetOutputEnable(bit, CTRUE);
+	}
+}
+
+void nxp_gpio_direction_input(int gpio)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	if (gpio < PAD_GPIO_ALV)
+		NX_GPIO_SetOutputEnable(grp, bit, CFALSE);
+	else
+		NX_ALIVE_SetOutputEnable(bit, CFALSE);
+}
+
+void nxp_gpio_set_value(int gpio, int val)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	if (gpio < PAD_GPIO_ALV)
+		NX_GPIO_SetOutputValue(grp, bit, (val ? CTRUE : CFALSE));
+	else
+		NX_ALIVE_SetOutputValue(bit, (val ? CTRUE : CFALSE));
+}
+
+int nxp_gpio_get_value(int gpio)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	if (gpio < PAD_GPIO_ALV)
+		return (int) NX_GPIO_GetInputValue(grp, bit);
+	else
+		return (int) NX_ALIVE_GetInputValue(bit);
+}
+
+void nxp_gpio_set_pull_enb(int gpio, int mode)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	NX_GPIO_SetPullEnable(grp, bit, mode);
+}
+
+void nxp_gpio_set_pull_sel(int gpio, int mode)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	NX_GPIO_SetPullSelect(grp, bit, mode);
+}
+
+void nxp_gpio_set_drv(int gpio, int mode)
+{
+	int drv1 = 0, drv0 = 0;
+	int drv1_value, drv0_value;
+	int grp, bit;
+
+	if (gpio > (PAD_GPIO_ALV - 1) )
+		return;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	switch (mode)
+	{
+	case GPIO_DRV_1X: drv0 = 0, drv1 = 0; break;
+	case GPIO_DRV_2X: drv0 = 0, drv1 = 1; break;
+	case GPIO_DRV_3X: drv0 = 1, drv1 = 0; break;
+	case GPIO_DRV_4X: drv0 = 1, drv1 = 1; break;
+	default: break;
+	}
+	DBGOUT("DRV Strength : GRP : i %x Bit: %x  mode :%d  \n   ",
+				grp, bit, mode);
+
+	drv1_value = NX_GPIO_GetDRV1(grp) & ~(1 << bit);
+	drv0_value = NX_GPIO_GetDRV0(grp) & ~(1 << bit);
+
+	if (drv1) drv1_value |= (drv1 << bit);
+	if (drv0) drv0_value |= (drv0 << bit);
+
+	DBGOUT(" Value : drv1 :%8x  drv0 %8x \n ", drv1_value, drv0_value);
+
+	NX_GPIO_SetDRV0 ( grp, drv0_value );
+	NX_GPIO_SetDRV1 ( grp, drv1_value );
+}
+
+int nxp_gpio_get_group(int gpio)
+{
+	return (int) ((gpio / 32) & 0x07);
+}
+
+int nxp_gpio_get_pin(int gpio)
+{
+	return (int) (gpio % 32);
+}
+
+
+
+/* Interrupt Interface  */
+
+void nxp_gpio_set_int_mode(int gpio, int d_mode)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	if (gpio < PAD_GPIO_ALV)
+	{
+		NX_GPIO_SetPadFunction(grp, bit, PAD_MODE_INT);
+		NX_GPIO_SetInterruptMode(grp, bit, d_mode);
+	}
+	else
+	{
+		NX_ALIVE_SetDetectMode( d_mode, bit, CTRUE );
+	}
+}
+
+void nxp_gpio_set_int_en(int gpio, int enable)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	if (gpio < PAD_GPIO_ALV)
+		NX_GPIO_SetInterruptEnable(grp, bit, enable);
+	else
+		NX_ALIVE_SetInterruptEnable(bit , enable);
+}
+
+
+int nxp_gpio_get_int_en(int gpio)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	if (gpio < PAD_GPIO_ALV)
+		return (int) NX_GPIO_GetInterruptEnable(grp, bit);
+	else
+		return (int) NX_ALIVE_GetInterruptEnable(bit);
+}
+
+int nxp_gpio_get_int_pend(int gpio)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	if (gpio < PAD_GPIO_ALV)
+		return (int) NX_GPIO_GetInterruptPending(grp, bit);
+	else
+		return (int) NX_ALIVE_GetInterruptPending(bit);
+}
+
+void nxp_gpio_set_int_clear(int gpio)
+{
+	int grp, bit;
+
+	grp = PAD_GET_GROUP(gpio);
+	bit = PAD_GET_BITNO(gpio);
+
+	if (gpio < PAD_GPIO_ALV)
+		NX_GPIO_ClearInterruptPending(grp, bit);
+	else
+		NX_ALIVE_ClearInterruptPending(bit);
+}
+
 /* Common GPIO API */
+
 int gpio_request(unsigned gpio, const char *label)
 {
 	return 0;
@@ -36,97 +267,24 @@ int gpio_free(unsigned gpio)
 
 int gpio_direction_input(unsigned gpio)
 {
-	int grp = PAD_GET_GROUP(gpio);
-	int bit = PAD_GET_BITNO(gpio);
-
-	if (grp < PAD_GET_GROUP(PAD_GPIO_ALV))
-		NX_GPIO_SetOutputEnable(grp, bit, CFALSE);
-	else
-		NX_ALIVE_SetOutputEnable(bit, CFALSE);
-
+	nxp_gpio_direction_input(gpio);
 	return 0;
 }
 
 int gpio_direction_output(unsigned gpio, int value)
 {
-	int grp = PAD_GET_GROUP(gpio);
-	int bit = PAD_GET_BITNO(gpio);
-
-	if (grp < PAD_GET_GROUP(PAD_GPIO_ALV)) {
-		NX_GPIO_SetOutputValue(grp, bit, (value ? CTRUE : CFALSE));
-		NX_GPIO_SetOutputEnable(grp, bit, CTRUE);
-	} else {
-		NX_ALIVE_SetOutputValue(bit, (value ? CTRUE : CFALSE));
-		NX_ALIVE_SetOutputEnable(bit, CTRUE);
-	}
+	nxp_gpio_direction_output(gpio, value);
 	return 0;
 }
 
 int gpio_get_value(unsigned gpio)
 {
-	int grp = PAD_GET_GROUP(gpio);
-	int bit = PAD_GET_BITNO(gpio);
-
-	if (grp < PAD_GET_GROUP(PAD_GPIO_ALV))
-		return (int) NX_GPIO_GetInputValue(grp, bit);
-	else
-		return (int) NX_ALIVE_GetInputValue(bit);
+	return nxp_gpio_get_value(gpio);
 }
 
 int gpio_set_value(unsigned gpio, int value)
 {
-	int grp = PAD_GET_GROUP(gpio);
-	int bit = PAD_GET_BITNO(gpio);
-
-	if (grp < PAD_GET_GROUP(PAD_GPIO_ALV))
-		NX_GPIO_SetOutputValue(grp, bit, (value ? CTRUE : CFALSE));
-	else
-		NX_ALIVE_SetOutputValue(bit, (value ? CTRUE : CFALSE));
+	nxp_gpio_set_value(gpio, value);
 
 	return 0;
-}
-
-/* Nexell GPIO API */
-int gpio_get_int_pend(int gpio)
-{
-	int grp = PAD_GET_GROUP(gpio);
-	int bit = PAD_GET_BITNO(gpio);
-
-	if (grp < PAD_GET_GROUP(PAD_GPIO_ALV))
-		return (int) NX_GPIO_GetInterruptPending(grp, bit);
-	else
-		return (int) NX_ALIVE_GetInterruptPending(bit);
-}
-
-void gpio_set_int_clear(int gpio)
-{
-	int grp = PAD_GET_GROUP(gpio);
-	int bit = PAD_GET_BITNO(gpio);
-
-	if (grp < PAD_GET_GROUP(PAD_GPIO_ALV))
-		NX_GPIO_ClearInterruptPending(grp, bit);
-	else
-		NX_ALIVE_ClearInterruptPending(bit);
-}
-
-void gpio_set_alt(int gpio, int mode)
-{
-	int grp = PAD_GET_GROUP(gpio);
-	int bit = PAD_GET_BITNO(gpio);
-
-	if (grp > (PAD_GET_GROUP(PAD_GPIO_ALV) - 1) )
-		return;
-
-	NX_GPIO_SetPadFunction(grp, bit, (NX_GPIO_PADFUNC)mode);
-}
-
-int gpio_get_alt(int gpio)
-{
-	int grp = PAD_GET_GROUP(gpio);
-	int bit = PAD_GET_BITNO(gpio);
-
-	if (grp > (PAD_GET_GROUP(PAD_GPIO_ALV) - 1) )
-		return -1;
-
-	return (int) NX_GPIO_GetPadFunction(grp, bit);
 }
--- include/micrel.h
+++ include/micrel.h
@@ -1,32 +1,55 @@
 #ifndef _MICREL_H
 
-#define MII_KSZ9021_EXT_COMMON_CTRL		0x100
+#define MII_KSZ9021_EXT_COMMON_CTRL			0x100
 #define MII_KSZ9021_EXT_STRAP_STATUS		0x101
 #define MII_KSZ9021_EXT_OP_STRAP_OVERRIDE	0x102
 #define MII_KSZ9021_EXT_OP_STRAP_STATUS		0x103
 #define MII_KSZ9021_EXT_RGMII_CLOCK_SKEW	0x104
 #define MII_KSZ9021_EXT_RGMII_RX_DATA_SKEW	0x105
 #define MII_KSZ9021_EXT_RGMII_TX_DATA_SKEW	0x106
-#define MII_KSZ9021_EXT_ANALOG_TEST		0x107
+#define MII_KSZ9021_EXT_ANALOG_TEST			0x107
 /* Register operations */
-#define MII_KSZ9031_MOD_REG			0x0000
+#define MII_KSZ9031_MOD_REG					0x0000
 /* Data operations */
 #define MII_KSZ9031_MOD_DATA_NO_POST_INC	0x4000
 #define MII_KSZ9031_MOD_DATA_POST_INC_RW	0x8000
 #define MII_KSZ9031_MOD_DATA_POST_INC_W		0xC000
 
+/* MMD Address 2h registers */
 #define MII_KSZ9031_EXT_RGMII_CTRL_SIG_SKEW	0x4
 #define MII_KSZ9031_EXT_RGMII_RX_DATA_SKEW	0x5
 #define MII_KSZ9031_EXT_RGMII_TX_DATA_SKEW	0x6
 #define MII_KSZ9031_EXT_RGMII_CLOCK_SKEW	0x8
+
+#define MII_KSZ9031_EXT_RGMII_RX_DV_SHIFT	4
+#define MII_KSZ9031_EXT_RGMII_TX_EN_SHIFT	0
+
+#define MII_KSZ9031_EXT_RGMII_RX3_SHIFT		12
+#define MII_KSZ9031_EXT_RGMII_RX2_SHIFT		8
+#define MII_KSZ9031_EXT_RGMII_RX1_SHIFT		4
+#define MII_KSZ9031_EXT_RGMII_RX0_SHIFT		0
+
+#define MII_KSZ9031_EXT_RGMII_TX3_SHIFT		12
+#define MII_KSZ9031_EXT_RGMII_TX2_SHIFT		8
+#define MII_KSZ9031_EXT_RGMII_TX1_SHIFT		4
+#define MII_KSZ9031_EXT_RGMII_TX0_SHIFT		0
+
+#define MII_KSZ9031_EXT_RGMII_GTX_CLK_SHIFT	5
+#define MII_KSZ9031_EXT_RGMII_RX_CLK_SHIFT	0
+
+/* MMD Address 7h registers */
+#define MII_KSZ9031_EXT_EEE_ADVERTISEMENT	0x3C
+#define MII_KSZ9031_EXT_EEE_1000BASE_SHIFT  2
+#define MII_KSZ9031_EXT_EEE_100BASE_SHIFT	1
+
 
 struct phy_device;
 int ksz9021_phy_extended_write(struct phy_device *phydev, int regnum, u16 val);
 int ksz9021_phy_extended_read(struct phy_device *phydev, int regnum);
 
 int ksz9031_phy_extended_write(struct phy_device *phydev, int devaddr,
-			       int regnum, u16 mode, u16 val);
+						int regnum, u16 mode, u16 val);
 int ksz9031_phy_extended_read(struct phy_device *phydev, int devaddr,
-			      int regnum, u16 mode);
+						int regnum, u16 mode);
 
 #endif

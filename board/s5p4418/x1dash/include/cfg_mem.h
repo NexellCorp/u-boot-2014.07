f5b603ba156cc4246eebedf4b538dd91c7ba5f30
allan.park <allan.park@nexell.co.kr>
2016-01-27 16:53:02 +0900

    [FIX] u-boot dram size to 1GB

--- board/s5p4418/x1dash/include/cfg_mem.h
+++ board/s5p4418/x1dash/include/cfg_mem.h
@@ -16,6 +16,6 @@
  * 	 System memory map
  */
 #define	CFG_MEM_PHY_SYSTEM_BASE			0x40000000	/* System, must be at an evne 2MB boundary (head.S) */
-#define	CFG_MEM_PHY_SYSTEM_SIZE			0x10000000	/* 1G MB */
+#define	CFG_MEM_PHY_SYSTEM_SIZE			0x40000000	/* 1G MB */
 
 #endif /* __CFG_MEM_H__ */
